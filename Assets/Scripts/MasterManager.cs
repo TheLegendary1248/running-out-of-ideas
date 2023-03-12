085f0e40251ecd262c62061ffc0cf4f55673c415
TheLegendary1248 <1248alex@gmail.com>
2023-03-12 00:40:22 -0500

    More pckg prune, Level Loading-ish

--- Assets/Scripts/MasterManager.cs
+++ Assets/Scripts/MasterManager.cs
@@ -5,9 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MasterManager : MonoBehaviour
 {
-    
+    /// <summary>
+    /// The scene the game loads in first
+    /// </summary>
+    public Scene initializerScene;
+    private void Start()
+    {
+        //Miscellanous code for testing already loaded scenes
+        if(SceneManager.sceneCount == 1)
+        {   //Game starts as usual. Loads the only scene that should be in the build index
+            SceneManager.LoadScene(0, LoadSceneMode.Additive);
+        }
+        else
+        {   //Game starts as usual- usually...
+
+        }
+        
+    }
     void LoadScene(string str)
     {
-
+        
     }
 }
