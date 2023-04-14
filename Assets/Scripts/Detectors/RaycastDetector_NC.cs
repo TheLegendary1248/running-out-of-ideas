1ec416d4f7fc0fd1125881d0a4372183016c3ec0
TheLegendary1248 <1248alex@gmail.com>
2023-04-14 16:57:13 -0400

    PARTIAL RAYCAST DETECTOR SCRIPT

--- Assets/Scripts/Detectors/RaycastDetector_NC.cs
+++ Assets/Scripts/Detectors/RaycastDetector_NC.cs
@@ -7,18 +7,65 @@ using UnityEngine;
 public class RaycastDetector_NC : MonoBehaviour
 {
     bool isActive;
-    float arcRange;
-    float radiusRange;
+    //Arc range of detector
+    [SerializeField]
+    float _arcRange;
+    public float arcRange { 
+        get => _arcRange;
+        set { _arcRange = value; /*PRECALC DOTRANGE*/ } }
+    //Radius range of detector
+    public float radiusRange;
+    //If obstructions should be considered
+    public bool doSightCheck;
+    //Precalculated float to be compared against the dot product of 
+    float dotRange;
+#if UNITY_EDITOR
+    Collider2D[] detectedCols;
+#endif
     private void FixedUpdate()
     {
-        //Get objects in AOE. Filter out terrain
+        //Get objects in AOE 
+        //TODO: FILTER OUT TERRAIN
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radiusRange);
         //Filter out objects not in arc range
+        Collider2D[] detectedColliders = new Collider2D[colliders.Length];
+        int detectedArraySize = 0; //TODO: Create Util for this 
+        bool skipArcCheck = arcRange >= 360f;
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Collider2D otherCollider = colliders[i];
+            
+            if (skipArcCheck) goto sightCheck; //Bite me, GOTO haters
+            Vector2 toObjNormal = (otherCollider.transform.position - transform.position).normalized;
+            Vector2 forwardNormal = transform.up;
+            //Compare
+            if (Vector2.Dot(toObjNormal, forwardNormal) > dotRange) continue;
+
+            sightCheck:
+            goto colliderAdd;
+            if (!doSightCheck) goto colliderAdd;
+            RaycastHit2D[] obstructions = Physics2D.LinecastAll(transform.position, otherCollider.transform.position);
+
+            colliderAdd:
+            detectedColliders[detectedArraySize++] = otherCollider;
+            detectedCols = detectedColliders;
+        }
         //Check objects line of sight, if used
         //Send out notifs
     }
     private void OnDrawGizmos()
     {
         //Draw helpy things here
+        if(detectedCols != null)
+        {
+            Gizmos.color = Color.yellow;
+            foreach(Collider2D collider in detectedCols)
+            {
+                if(collider) Gizmos.DrawLine(transform.position, collider.transform.position);
+            }
+        }
+        Gizmos.color = new Color(0f, 0.5f, 1f, 0.2f);
+        Gizmos.DrawWireSphere(transform.position, radiusRange);
     }
 
 }
--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -6,4 +6,6 @@ using UnityEngine;
 /// </summary>
 public static class Utils
 {
+
+    public static void LineCastChildChecks() { }
 }
