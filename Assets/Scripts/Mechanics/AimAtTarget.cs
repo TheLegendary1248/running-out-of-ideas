4cf8623044c147c21bf0d56d6f2ab62a1976c2de
TheLegendary1248 <1248alex@gmail.com>
2023-04-16 14:45:53 -0400

    Full Aim At Target

--- Assets/Scripts/Mechanics/AimAtTarget.cs
+++ Assets/Scripts/Mechanics/AimAtTarget.cs
@@ -11,17 +11,33 @@ public class AimAtTarget : MonoBehaviour
     [Tooltip("The rate at which the component faces the target, in degrees")]
     public float turnRate;
     /// <summary>
-    /// The margin of angle before the turret bothers rotating again
+    /// The margin of angle before the object bothers rotating again
     /// </summary>
-    public float margin;
+    public float beginMargin;
+    /// <summary>
+    /// 
+    /// </summary>
+    bool rotating = false;
     private void FixedUpdate()
     {
+        //Precalc
         Vector2 dif = target - (Vector2)transform.position;
-        dif = Utils.Vec2RightParallel(dif); //Because unity is wierd with these things
-        float angle = Mathf.MoveTowardsAngle(
-            transform.eulerAngles.z,
-            Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg,
-            turnRate);
-        transform.eulerAngles = new Vector3(0f, 0f, angle);
+        float dot = Vector2.Dot(dif, transform.up);
+
+        //If already rotating or vector difference is larger than margin
+        if(rotating | (dif.magnitude - dot) > beginMargin)
+        {
+            rotating = true;
+            dif = Utils.Vec2RightParallel(dif); //Because unity is wierd with these things
+            float turnDelta = turnRate * Time.fixedDeltaTime;
+            float angle = Mathf.MoveTowardsAngle(
+                transform.eulerAngles.z,
+                Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg,
+                turnDelta);
+            //Angle difference check. Might remove hardcode value
+            float angleDif = Mathf.DeltaAngle(angle, transform.eulerAngles.z);
+            if (Mathf.Abs(angleDif) < turnDelta / 2f) rotating = false;
+            transform.eulerAngles = new Vector3(0f, 0f, angle);
+        }
     }
 }
