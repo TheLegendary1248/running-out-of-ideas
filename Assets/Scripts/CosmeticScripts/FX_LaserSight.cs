6cb0d7d19ccac13964a65c364d2a583413494066
TheLegendary1248 <1248alex@gmail.com>
2023-04-16 15:14:11 -0400

    Laser Sight Script

--- Assets/Scripts/CosmeticScripts/FX_LaserSight.cs
+++ Assets/Scripts/CosmeticScripts/FX_LaserSight.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class FX_LaserSight : MonoBehaviour
 {
+    GameObject sight;
+    LineRenderer lineRender;
+    public Material material;
+    public float width;
+    public float range;
+    public Gradient color;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
+        sight = new GameObject("LaserSight", typeof(LineRenderer));
+        lineRender = sight.GetComponent<LineRenderer>();
+        lineRender.positionCount = 2;
+        lineRender.widthMultiplier = width;
+        lineRender.material = material;
+        lineRender.colorGradient = color;
 
-    // Update is called once per frame
-    void Update()
+    }
+    private void FixedUpdate()
     {
-        
+        Vector3 up = transform.up;
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, up, range);
+        lineRender.SetPositions(new Vector3[] {
+            transform.position,
+            ray ? ray.point : transform.position + (up * range)
+        });
+    }
+    private void OnDestroy()
+    {
+        Destroy(sight);
     }
 }
