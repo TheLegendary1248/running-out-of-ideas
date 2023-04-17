a5a4eec15fb9992e523d996d58f1b106580fe8fb
TheLegendary1248 <1248alex@gmail.com>
2023-04-17 19:07:52 -0400

    Arc Shader

--- Assets/Scripts/Detectors/RCDetectorEditor.cs
+++ Assets/Scripts/Detectors/RCDetectorEditor.cs
@@ -11,7 +11,7 @@ public class RCDetectorEditor : Editor
         RaycastDetector_NC[] scripts = targets.Cast<RaycastDetector_NC>().ToArray();
         float val = scripts[0].arcRange;
         bool allSame = scripts.CheckAlls(val => val.arcRange);
-        val = EditorGUILayout.Slider("Arc Range", val, 0, 360f);
+        val = EditorGUILayout.Slider("Arc Range", allSame ? val : float.NaN, 0, 360f);
         for (int i = 0; i < scripts.Length; i++)
         {
             scripts[i].arcRange = val; 
